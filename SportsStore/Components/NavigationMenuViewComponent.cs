4cccc12704aa790eefdfdd07228ad465f6aaa31a
oude91kiron <oa45@students.kiron.ngo>
2024-07-08 11:35:05 +0300

    Fix navigation menu view component.

--- SportsStore/Components/NavigationMenuViewComponent.cs
+++ SportsStore/Components/NavigationMenuViewComponent.cs
@@ -3,19 +3,24 @@ using SportsStore.Models;
 
 namespace SportsStore.Components
 {
-
+    // ViewComponent to generate the navigation menu
     public class NavigationMenuViewComponent : ViewComponent
     {
         private IStoreRepository repository;
 
+        // Constructor that initializes the repository
         public NavigationMenuViewComponent(IStoreRepository repo)
         {
             repository = repo;
         }
 
+        // Method to invoke the ViewComponent
         public IViewComponentResult Invoke()
         {
+            // Set the selected category in the ViewBag from the route data
             ViewBag.SelectedCategory = RouteData?.Values["category"];
+
+            // Return the view with distinct categories from the repository, ordered by category name
             return View(repository.Products
                 .Select(x => x.Category)
                 .Distinct()
