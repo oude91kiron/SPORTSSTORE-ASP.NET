70cdbff3335df3692ef31b5e157acbb3aaba4603
oude91kiron <oa45@students.kiron.ngo>
2024-07-24 11:06:39 +0300

    Creating Cart Summery Component.

---
+++ SportsStore/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Components
+{
+    /// <summary>
+    /// Component class represent cart summary.
+    /// </summary>
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private readonly Cart? cart;
+
+        public CartSummaryViewComponent(Cart cartService)
+        {
+            cart = cartService;
+        }
+
+        // Invoke cart object to view.
+        public IViewComponentResult Invoke()
+        {
+            return View(cart);
+        }
+    }
+}
